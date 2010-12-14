9b0367a1d43668f9e360131dc5ebced70a1433dd
Julien Biard <julien.biard@gmail.com>
2010-12-14 16:46:08 +0100

    descendants fixed

--- lib/mongoid_acts_as_tree.rb
+++ lib/mongoid_acts_as_tree.rb
@@ -133,10 +133,7 @@ module Mongoid
 
 				def descendants
 					# workorund for mongoid unexpected behavior
-					_new_record_var = self.instance_variable_get(:@new_record)
-					_new_record = _new_record_var != false
-
-					return [] if _new_record
+					return [] if !!self.instance_variable_get(:@new_record)
 					acts_as_tree_options[:class].all_in(path_field => [self._id]).order_by tree_order
 				end
 
